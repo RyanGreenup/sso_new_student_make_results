74c119f2e882fd68856f217132d541a53a43b0f1
Ryan Greenup <ryanxgreenup@gmail.com>
2025-02-11 16:17:05 +1100

    allow killing it

--- sso_new_student/main.py
+++ sso_new_student/main.py
@@ -7,31 +7,19 @@ import seaborn as sns
 from PySide6.QtWidgets import QApplication
 from .main_window import MainWindow
 import pandas as pd
+import signal
 
 app = typer.Typer(pretty_exceptions_enable=False)
 
 @app.command()
 def main() -> None:
-    dfs: dict[str, pd.DataFrame] = make_dataframe('EMC', '38000', '10', 'N5', Path("/tmp/"))
-    # Pull out the first df:
-    keys = list(dfs.keys())
-    if len(keys) <= 0:
-        print("No Dataframes returned", file = sys.stderr)
-    if (df := dfs.get(keys[0])) is None:
-        print("No Dataframe available, this is a bug")
 
-    for campus, df in dfs.items():
-        df = df
-        print(campus)
-        print(df.to_markdown())
-
-    df = pd.read_csv("/home/ryan/Downloads/working/newresultsEMC.csv")
-
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
     # Create the application
     app = QApplication(sys.argv)
 
     # Create and show the main window
-    window = MainWindow(df)
+    window = MainWindow()
     window.resize(800, 600)
     window.show()
 
